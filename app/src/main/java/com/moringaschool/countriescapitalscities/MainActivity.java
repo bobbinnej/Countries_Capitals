a2940b9d5d8161da04ba7e491f26b590ab4124e0
jackie <bobbinnej@gmail.com>
2022-05-29 15:54:51 +0300

    Create an intent to move from activitymain view to the allcountries view

--- app/src/main/java/com/moringaschool/countriescapitalscities/MainActivity.java
+++ app/src/main/java/com/moringaschool/countriescapitalscities/MainActivity.java
@@ -2,10 +2,12 @@ package com.moringaschool.countriescapitalscities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -13,29 +15,30 @@ import java.util.ArrayList;
 import java.util.List;
 import android.widget.AdapterView.OnItemSelectedListener;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
-public class MainActivity extends AppCompatActivity implements OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity  {
     //spinner element using butterknife
-//    @BindView(R.id.searchSpinner) Spinner searchSpinner;
+    @BindView(R.id.allCountriesButton)Button getStarted;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, AllCountries.class);
+            }
+        });
 
 
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String message=parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), message, Toast.LENGTH_SHORT).show();
-    }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }

2efd2d7b33d2fc49315920075c09956631f20786
jackie <bobbinnej@gmail.com>
2022-05-29 13:52:11 +0300

    Implement spinner to the search bar

--- app/src/main/java/com/moringaschool/countriescapitalscities/MainActivity.java
+++ app/src/main/java/com/moringaschool/countriescapitalscities/MainActivity.java
@@ -3,17 +3,44 @@ package com.moringaschool.countriescapitalscities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+import android.widget.AdapterView.OnItemSelectedListener;
+import butterknife.BindView;
+
+
+public class MainActivity extends AppCompatActivity implements OnItemSelectedListener {
+    //spinner element using butterknife
+//    @BindView(R.id.searchSpinner) Spinner searchSpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner searchSpinner=(Spinner) findViewById(R.id.searchSpinner);
+        Spinner searchSpinner = findViewById(R.id.searchSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.countries, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        searchSpinner.setAdapter(adapter);
+        searchSpinner.setOnItemSelectedListener(this);
 
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String message=parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }

6461605f9fdf451a26a8e53bcee24b8a69016315
jackie <bobbinnej@gmail.com>
2022-05-29 16:14:15 +0300

    Style the activity_all_countries.xml

--- app/src/main/java/com/moringaschool/countriescapitalscities/MainActivity.java
+++ app/src/main/java/com/moringaschool/countriescapitalscities/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity  {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(MainActivity.this, AllCountries.class);
+                startActivity(intent);
             }
         });
 

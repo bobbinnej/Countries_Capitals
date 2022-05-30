5169544011895bd01913011df1da28404c323b7e
jackie <bobbinnej@gmail.com>
2022-05-30 08:04:38 +0300

    Add unecessary files to .gitignore file

--- app/src/main/java/com/moringaschool/countriescapitalscities/ItemsHolder.java
+++ app/src/main/java/com/moringaschool/countriescapitalscities/ItemsHolder.java
@@ -1,5 +1,38 @@
 package com.moringaschool.countriescapitalscities;
 
 public class ItemsHolder {
+    //declare our variables
+    private int image;
+    private String country;
+    private String capital;
 
+    public ItemsHolder(int image, String country, String capital) {
+        this.image = image;
+        this.country = country;
+        this.capital = capital;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
 }
